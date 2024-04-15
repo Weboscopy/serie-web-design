16eabe03090592cad4d5bef94b9233645c128194
Weboscopy <weboscopy@gmail.com>
2024-04-15 17:33:15 +0200

    tuto 07 Dark Mode

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const progress = document.querySelector(".progress")
+const toggler = document.getElementById("toggler")
 
-const app = {
-    percentage: 0,
-    max: 100,
-    update(){
-        progress.style.background = `conic-gradient(teal ${app.percentage * 3.6}deg, #eee 0deg)`
-        progress.firstElementChild.innerText = `${app.percentage} %`
-        if(app.percentage < app.max){
-            setTimeout(() => {
-                app.percentage += 5 
-                app.update()
-            }, 50)
+const initApp = () => {
+    let mode = JSON.parse(localStorage.getItem("mode")) ?? false 
+
+    toggler.addEventListener("change", () => {
+        mode = !mode 
+        setTheme()
+    })
+
+    function setTheme(){
+        if(mode){
+            document.body.style.backgroundColor = "#333"
+            toggler.checked = true 
+            localStorage.setItem("mode", JSON.stringify(true))
+        } else {
+            document.body.style.backgroundColor = "white"
+            toggler.checked = false 
+            localStorage.setItem("mode", JSON.stringify(false))
         }
     }
+
+    setTheme()
 }
 
-document.addEventListener("DOMContentLoaded", app.update)
+document.addEventListener("DOMContentLoaded", initApp)
