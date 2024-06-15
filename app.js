5d6b4fd0a8d0884c9c8b8b2fe85e7bee9fda4b1a
Weboscopy <weboscopy@gmail.com>
2024-06-15 18:39:44 +0200

    tuto 13 sur les cards (flexbox)

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const hamburger = document.querySelector(".hamburger")
-const nav = document.querySelector("nav")
+const container = document.getElementById("container")
+const url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
 
 const app = () => {
-
-    const toggleMenu = () => {
-        hamburger.classList.toggle("active")
-        nav.classList.toggle("mobile")
+    const fetchData = async (url) => {
+     const {drinks} =  await ( await fetch(url)).json()
+     displayData(drinks)
     }
 
-    hamburger.addEventListener("click", toggleMenu)
+    const displayData = (data) => {
+        container.innerHTML = ''
+        data?.forEach(cocktail => {
+            const card = document.createElement("div")
+            card.classList.add("card")
+            card.innerHTML = `
+             <img src="${cocktail.strDrinkThumb}">
+             <div class="overlay">
+                ${cocktail.strDrink}
+             </div>
+            `
+            container.appendChild(card)
+        })
+    }
+
+    fetchData(url)
 }
 
 document.addEventListener("DOMContentLoaded", app)
