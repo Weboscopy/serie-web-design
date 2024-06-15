9c304dba9f80330ffc9dd8af6f1e60ef8ef9a74e
Weboscopy <weboscopy@gmail.com>
2024-06-15 18:44:40 +0200

    tuto 16 - effet machine à écrire

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const hamburgerBtn = document.querySelector("button")
-const mobileMenu = document.querySelector("#mobile-menu")
+const el = document.querySelector("h2")
 
-const initApp = () => {
+const words = JSON.parse(el.getAttribute("data-words"))
 
-    const toggle = () => {
-        hamburgerBtn.classList.toggle("btn-active")
-        mobileMenu.classList.toggle("mobile-active")
+const app = {
+    txt: "",
+    wordIndex: 0,
+    isDeleting: false,
+    speed: 500,
+    init(){
+        const currentIndex = app.wordIndex % words.length 
+        const word = words[currentIndex]
+
+        if(app.isDeleting){
+          app.txt = word.substring(0, app.txt.length - 1)
+        } else {
+           app.txt = word.substring(0, app.txt.length + 1)
+        }
+
+        el.innerText = app.txt
+
+        if(app.isDeleting){
+            app.speed /= 2
+        }
+
+        if(!app.isDeleting && app.txt === word){
+            app.speed = 3000 
+            app.isDeleting = true 
+        } else if(app.isDeleting && app.txt === "") {
+            app.isDeleting = false 
+            app.wordIndex++
+            app.speed = 500
+        }
+
+        setTimeout(() => app.init(), app.speed)
     }
-
-    hamburgerBtn.addEventListener("click", toggle)
-    mobileMenu.addEventListener("click", toggle)
 }
 
-document.addEventListener("DOMContentLoaded", initApp)
+
+document.addEventListener("DOMContentLoaded", app.init)
+
