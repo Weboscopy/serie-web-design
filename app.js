84f7bb614f45f8418cf800ef61c7f488457d1a10
Weboscopy <weboscopy@gmail.com>
2024-06-15 18:49:08 +0200

    tuto 19 sur les tags

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-const progressBar = document.querySelector(".progressBar")
-const next = document.getElementById("next")
-const prev = document.getElementById("prev")
-const steps = document.querySelectorAll(".step")
+const ul = document.querySelector("ul")
+const input = document.querySelector("input")
+const counter = document.querySelector("span")
+
+
+const maxTags = 10
+let tags = []
 
 const app = {
-    current: 1,
     init() {
-        next.addEventListener("click", () => {
-            if (app.current < steps.length) {
-                app.current++
-                app.update()
-            }
-        })
-
-        prev.addEventListener("click", () => {
-            if (app.current > 1) {
-                app.current--
-                app.update()
-            }
-        })
+        input.addEventListener("keyup", app.addTag)
+        ul.addEventListener("click", app.removeTag)
+        app.countTags()
     },
-    update() {
-        steps.forEach((step, index) => {
-            (index < app.current) ? step.classList.add("active") : step.classList.remove("active")
+    addTag(e) {
+        if (e.key == "Enter" || e.key == ",") {
+            let tag = e.target.value.replace(",", "").replace(/\s+/g, " ")
 
-            const actives = document.querySelectorAll(".active")
-
-            progressBar.style.width = (actives.length - 1) / (steps.length - 1) * 100 + "%"
-
-            if (app.current == 1) {
-                prev.disabled = true
-            } else if (app.current == steps.length) {
-                next.disabled = true
-            } else {
-                prev.disabled = false
-                next.disabled = false
+            if (tag.length > 10) {
+                tag = tag.substring(0, 10) + "..."
             }
+
+            if (tag.length > 3 && !tags.includes(tag)) {
+                if (tags.length < maxTags) {
+                    tags.push(tag)
+                    app.displayDOMTags()
+                }
+            }
+
+            e.target.value = ""
+            input.focus()
+        }
+    },
+    displayDOMTags() {
+        ul.querySelectorAll("li").forEach(li => li.remove())
+        tags.forEach(tag => {
+            const li = `<li>${tag} <i class="fa fa-times"></i></li>`
+            input.insertAdjacentHTML("beforebegin", li)
         })
+        app.countTags()
+    },
+    removeTag(e) {
+        if (e.target.tagName === "I") {
+            tags = tags.filter(item => item !== e.target.parentElement.innerText)
+            app.displayDOMTags()
+        }
+    },
+    countTags() {
+        counter.innerText = maxTags - tags.length
     }
 }
+
 
 document.addEventListener("DOMContentLoaded", app.init)
