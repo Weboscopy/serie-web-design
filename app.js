5c56693de6f21da1da8f41f2960630932151d0ef
Weboscopy <weboscopy@gmail.com>
2024-08-15 10:53:19 +0200

    tuto 25 validation instantannée

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
-const forms = document.querySelectorAll("form")
-const prevs = document.querySelectorAll(".prev")
-const nexts = document.querySelectorAll(".next")
-const listItems = document.querySelectorAll("li")
-const ul = document.querySelector("ul")
+const inputs = document.querySelectorAll("input")
+const password = document.querySelector("#password")
+const confirm_password = document.querySelector("#confirm_password")
+const submitBtn = document.querySelector("button")
 
-const app = {
-    step: 0,
-    init() {
-        nexts.forEach(next => {
-            next.addEventListener("click", () => {
-                app.step++
-                app.changeForm()
-            })
-        })
 
-        prevs.forEach(prev => {
-            prev.addEventListener("click", () => {
-                app.step--
-                app.changeForm()
-            })
-        })
+const initApp = () => {
 
-        listItems.forEach((item, index) => {
-            item.addEventListener("click", () => {
-                app.step = index
-                app.changeForm()
-            })
-        })
-    },
-    changeForm() {
-        forms.forEach(form => {
-            form.style.transform = `translateX(${-app.step * 100}%)`
-        })
-
-        listItems.forEach(item => {
-            item.classList.remove("active")
-        })
-
-        if (app.step !== 3) {
-            listItems[app.step].classList.add("active")
-        } else {
-            ul.style.justifyContent = "center"
-            ul.innerHTML = "<h2>Terminé</h2>"
-        }
+    const expressions = {
+        username: /^[A-Za-z\d]{5,10}$/,
+        password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})$/,
+        phone: /^(0|\+33)[1-9]([-.: ]?[0-9]{2}){4}$/,
     }
+
+    const form = {
+        username: "",
+        email: "",
+        password: "",
+        confirm_password: "",
+        phone: ""
+    }
+
+    function validate(field, regex) {
+        if (field.name !== "confirm_password") {
+            if (regex.test(field.value)) {
+                field.parentElement.classList.remove("invalid")
+                form[field.name] = field.value
+            } else {
+                field.parentElement.classList.add("invalid")
+                form[field.name] = ""
+            }
+        }
+
+        if (password.value !== confirm_password.value && (password.value !== "" || confirm_password.value !== "")) {
+            confirm_password.parentElement.classList.add("invalid")
+            form.confirm_password = ""
+        } else {
+            confirm_password.parentElement.classList.remove("invalid")
+            form.confirm_password = confirm_password.value
+        }
+
+
+        if (Object.values(form).every(val => val !== "")) {
+            submitBtn.disabled = false
+        }
+
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("keyup", (e) => {
+            validate(e.target, expressions[e.target.attributes.name.value])
+        })
+    })
+
+    submitBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        if (Object.values(form).every(val => val !== "")) {
+            console.log(form)
+            document.querySelector("h2").classList.add("valid")
+        }
+    })
+
 }
 
-document.addEventListener("DOMContentLoaded", app.init)
+document.addEventListener("DOMContentLoaded", initApp)
