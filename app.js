826b6accd15ebd6043e53e1811008266f963224f
Weboscopy <weboscopy@gmail.com>
2024-08-15 10:52:07 +0200

    tuto 24 Formulaire à étapes

--- app.js
+++ app.js
@@ -1,74 +1,49 @@
-let items = document.querySelectorAll(".item")
-let leftBox = document.getElementById("left")
-let rightBox = document.getElementById("right")
+const forms = document.querySelectorAll("form")
+const prevs = document.querySelectorAll(".prev")
+const nexts = document.querySelectorAll(".next")
+const listItems = document.querySelectorAll("li")
+const ul = document.querySelector("ul")
 
-
-const initApp = () => {
-    items.forEach(item => {
-        item.addEventListener("dragstart", (e) => {
-            let selectedItem = e.target
-            selectedItem.classList.add("hold")
-
-            selectedItem.addEventListener("dragend", (e) => {
-                if (selectedItem) {
-                    selectedItem.classList.remove("hold")
-                }
-                selectedItem = null
+const app = {
+    step: 0,
+    init() {
+        nexts.forEach(next => {
+            next.addEventListener("click", () => {
+                app.step++
+                app.changeForm()
             })
-
-            rightBox.addEventListener("drop", (e) => {
-                if (selectedItem) {
-                    rightBox.appendChild(selectedItem)
-                    selectedItem.classList.remove("hold")
-                    rightBox.classList.remove("hovered")
-                }
-                selectedItem = null
-            })
-
-            rightBox.addEventListener("dragover", (e) => {
-                e.preventDefault()
-            })
-
-
-            leftBox.addEventListener("drop", (e) => {
-                if (selectedItem) {
-                    leftBox.appendChild(selectedItem)
-                    selectedItem.classList.remove("hold")
-                    leftBox.classList.remove("hovered")
-                }
-
-                selectedItem = null
-            })
-
-            leftBox.addEventListener("dragover", (e) => {
-                e.preventDefault()
-            })
-
-            rightBox.addEventListener("dragenter", (e) => {
-                setTimeout(() => {
-                    rightBox.classList.add("hovered")
-                }, 0)
-
-            })
-
-            leftBox.addEventListener("dragenter", (e) => {
-                setTimeout(() => {
-                    leftBox.classList.add("hovered")
-                }, 0)
-            })
-
-
-            rightBox.addEventListener("dragleave", (e) => {
-                rightBox.classList.remove("hovered")
-            })
-
-            leftBox.addEventListener("dragleave", (e) => {
-                leftBox.classList.remove("hovered")
-            })
-
-
         })
-    })
+
+        prevs.forEach(prev => {
+            prev.addEventListener("click", () => {
+                app.step--
+                app.changeForm()
+            })
+        })
+
+        listItems.forEach((item, index) => {
+            item.addEventListener("click", () => {
+                app.step = index
+                app.changeForm()
+            })
+        })
+    },
+    changeForm() {
+        forms.forEach(form => {
+            form.style.transform = `translateX(${-app.step * 100}%)`
+        })
+
+        listItems.forEach(item => {
+            item.classList.remove("active")
+        })
+
+        if (app.step !== 3) {
+            listItems[app.step].classList.add("active")
+        } else {
+            ul.style.justifyContent = "center"
+            ul.innerHTML = "<h2>Terminé</h2>"
+        }
+    }
 }
 
-document.addEventListener("DOMContentLoaded", initApp)
+document.addEventListener("DOMContentLoaded", app.init)
