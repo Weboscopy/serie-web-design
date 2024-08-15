b771edb6da337d30bc09b9ebfef38957d63f2b9a
Weboscopy <weboscopy@gmail.com>
2024-08-15 10:56:48 +0200

    tuto 26 système de notation

--- app.js
+++ app.js
@@ -1,66 +1,110 @@
-const inputs = document.querySelectorAll("input")
-const password = document.querySelector("#password")
-const confirm_password = document.querySelector("#confirm_password")
-const submitBtn = document.querySelector("button")
-
-
-const initApp = () => {
-
-    const expressions = {
-        username: /^[A-Za-z\d]{5,10}$/,
-        password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})$/,
-        phone: /^(0|\+33)[1-9]([-.: ]?[0-9]{2}){4}$/,
+const data = [
+    {
+        "id": 1,
+        "img": "./images/img1.jpg",
+        "title": "Super maison",
+        "desc": "maison moderne avec plein d'éléments super sympa",
+        "stars": 2
+    },
+    {
+        "id": 2,
+        "img": "./images/img2.jpg",
+        "title": "Chalet magnifique",
+        "desc": "Chalet spacieuse dotée de finitions haut de gamme.",
+        "stars": 1
+    },
+    {
+        "id": 3,
+        "img": "./images/img3.jpg",
+        "title": "Appartement hyper center",
+        "desc": "Appartement lumineux avec une déco vraiment top",
+        "stars": 3
+    },
+    {
+        "id": 4,
+        "img": "./images/img4.jpg",
+        "title": "Maison de campagne chaleureuse",
+        "desc": "Maison typique avec une ambiance cosy",
+        "stars": 5
+    },
+    {
+        "id": 5,
+        "img": "./images/img5.jpg",
+        "title": "Demeure provençale",
+        "desc": "Demeure traditionnelle du Sud avec terasse ensoleillée",
+        "stars": 4
+    },
+    {
+        "id": 6,
+        "img": "./images/img6.jpg",
+        "title": "Loft design",
+        "desc": "Loft design avec des touches contemporaines et élégantes",
+        "stars": 3
     }
+]
 
-    const form = {
-        username: "",
-        email: "",
-        password: "",
-        confirm_password: "",
-        phone: ""
-    }
 
-    function validate(field, regex) {
-        if (field.name !== "confirm_password") {
-            if (regex.test(field.value)) {
-                field.parentElement.classList.remove("invalid")
-                form[field.name] = field.value
-            } else {
-                field.parentElement.classList.add("invalid")
-                form[field.name] = ""
+const container = document.getElementById("container")
+
+
+const app = {
+    init() {
+        app.renderList()
+    },
+    renderList() {
+        container.innerHTML = data.map(item => `
+            <div class="card">
+    <div class="featured">
+        <img src="${item.img}" />
+    </div>
+    <div class="details">
+        <div class="info">
+            <h4>${item.title}</h4>
+            <p>${item.desc}</p>
+        </div>
+        <div class="rating">
+            <div class="stars" data-id="${item.id}">
+                ${item.stars === 5 ?
+                `<span data-star="5" data-selected="true">★</span>` :
+                `<span data-star="5">★</span>`
             }
-        }
+                ${item.stars === 4 ?
+                `<span data-star="4" data-selected="true">★</span>` :
+                `<span data-star="4">★</span>`
+            }
+                ${item.stars === 3 ?
+                `<span data-star="3" data-selected="true">★</span>` :
+                `<span data-star="3">★</span>`
+            }
+                ${item.stars === 2 ?
+                `<span data-star="2" data-selected="true">★</span>` :
+                `<span data-star="2">★</span>`
+            }
+                ${item.stars === 1 ?
+                `<span data-star="1" data-selected="true">★</span>` :
+                `<span data-star="1">★</span>`
+            }
+            </div>
+        </div>
+    </div>
+</div>
+            `)
 
-        if (password.value !== confirm_password.value && (password.value !== "" || confirm_password.value !== "")) {
-            confirm_password.parentElement.classList.add("invalid")
-            form.confirm_password = ""
-        } else {
-            confirm_password.parentElement.classList.remove("invalid")
-            form.confirm_password = confirm_password.value
-        }
+        const starsDiv = document.querySelectorAll(".stars")
 
-
-        if (Object.values(form).every(val => val !== "")) {
-            submitBtn.disabled = false
-        }
-
-    }
-
-    inputs.forEach(input => {
-        input.addEventListener("keyup", (e) => {
-            validate(e.target, expressions[e.target.attributes.name.value])
+        starsDiv.forEach(div => {
+            div.addEventListener("click", app.update)
         })
-    })
-
-    submitBtn.addEventListener("click", (e) => {
-        e.preventDefault()
-        if (Object.values(form).every(val => val !== "")) {
-            console.log(form)
-            document.querySelector("h2").classList.add("valid")
+    },
+    update(e) {
+        if (e.target.tagName === "SPAN") {
+            const rating = +e.target.dataset.star
+            const id = +e.target.parentElement.dataset.id
+            const item = data.find(item => item.id === id)
+            item.stars = rating
+            app.renderList()
         }
-    })
-
+    }
 }
 
-document.addEventListener("DOMContentLoaded", initApp)
+document.addEventListener("DOMContentLoaded", app.init)
