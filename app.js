4e28d10ebad4c7cfb80c6335872751afb03f4231
Weboscopy <weboscopy@gmail.com>
2024-08-15 10:46:06 +0200

    tuto 21 carrousel

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-const inputs = document.querySelectorAll("input")
-const button = document.querySelector("button")
-const container = document.querySelector(".container")
+const slideContainer = document.querySelector(".slide-container")
+const leftBtn = document.querySelector("#left")
+const rightBtn = document.querySelector("#right")
+const slides = document.querySelectorAll(".slide")
 
-let code = []
-const correct_code = [2, 4, 6, 8]
+const intervalTime = 4000
+let currentSlide = 0
+let interval = setInterval(autoPlay, intervalTime)
 
-const app = {
-  init() {
-    button.addEventListener("click", app.verify)
-    inputs[0].focus()
-    inputs.forEach((input, index) => {
-      input.addEventListener("keydown", (e) => {
-        if (e.key >= 0 && e.key <= 9) {
-          code[index] = Number(e.key)
-          inputs[index].value = ""
-          setTimeout(() => app.focusInput(inputs[index + 1]), 10)
-        } else if (e.key === "Backspace") {
-          code.splice(index, 1)
-          setTimeout(() => app.focusInput(inputs[index - 1]), 10)
-        }
-      })
-    })
-  },
-  focusInput(input) {
-    if (input) {
-      input.focus()
-    }
-  },
-  verify() {
-    const p = document.createElement("p")
-    if (code.length == inputs.length && code.every((v, i) => v == correct_code[i])) {
-      p.innerText = "Code valide"
-      p.classList.add("success")
-    } else {
-      p.innerText = "Code invalide"
-      p.classList.add("error")
-    }
+leftBtn.addEventListener("click", () => {
+    currentSlide--
+    changeSlide()
+    resetInterval()
+})
 
-    container.appendChild(p)
-    setTimeout(() => p.remove(), 2000)
-  }
+rightBtn.addEventListener("click", () => {
+    currentSlide++
+    changeSlide()
+    resetInterval()
+})
+
+function autoPlay() {
+    currentSlide++
+    changeSlide()
 }
 
-document.addEventListener("DOMContentLoaded", app.init)
+
+function changeSlide() {
+    if (currentSlide > slides.length - 1) {
+        currentSlide = 0
+    } else if (currentSlide < 0) {
+        currentSlide = slides.length - 1
+    }
+
+    slideContainer.style.transform = `translateX(${-currentSlide * 100}vw)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(autoPlay, intervalTime)
+}
